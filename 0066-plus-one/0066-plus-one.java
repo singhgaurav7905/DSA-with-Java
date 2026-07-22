2365a1402af1fb30d27fd784418cf372e35b5229
Gaurav Singh <singhgaurav7905@gmail.com>
2026-07-22 20:50:30 +0530

    Time: 0 ms (100%), Space: 43.2 MB (90.87%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length - 1 ;
+
+        for(int i = n ; i >= 0 ; i--){
+
+        if(digits[i] < 9){
+         digits[i] = digits[i] + 1 ; 
+         return digits ;
+        }
+        digits[i] = 0 ;
+        }
+
+        int[] arr = new int [digits.length + 1];
+        arr[0] = 1 ;
+        return arr ;
+    }
+}
