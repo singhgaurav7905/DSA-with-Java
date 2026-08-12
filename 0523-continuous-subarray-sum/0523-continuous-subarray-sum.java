f544f814180899770ddcf3a3b98a503d01ea7630
Gaurav Singh <singhgaurav7905@gmail.com>
2026-08-12 18:23:01 +0530

    Time: 25 ms (42.85%), Space: 139.7 MB (49.75%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -3,6 +3,7 @@ class Solution {
         
         int sum = 0 ;
         Map<Integer , Integer> map = new HashMap<>();
+        map.put(0,-1) ;
         for(int i = 0 ; i < nums.length ; i++){
             sum += nums[i] ;
 
